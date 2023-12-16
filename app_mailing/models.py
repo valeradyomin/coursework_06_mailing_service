56b9100a188295e481ac92c60dd845456069ecbf
Valera Demin <wahrwolffe@gmail.com>
2023-12-17 00:23:52 +0300

    add Client model

--- app_mailing/models.py
+++ app_mailing/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {
+    'null': True,
+    'blank': True,
+}
+
+
+class Client(models.Model):
+    email = models.EmailField(max_length=100, unique=True, verbose_name='почтовый адрес')
+    initials = models.CharField(max_length=50, verbose_name='инициалы')
+    comment = models.TextField(verbose_name='комментарий', **NULLABLE)
