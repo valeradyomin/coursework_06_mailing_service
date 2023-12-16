60d29593708d32c814e171a008cd7622891ccecd
Valera Demin <wahrwolffe@gmail.com>
2023-12-16 21:35:40 +0300

    db sets

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +74,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+load_dotenv()
+secret = os.getenv('DB_PASS')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mailing_service',
+        'USER': 'postgres',
+        'PASSWORD': secret,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
