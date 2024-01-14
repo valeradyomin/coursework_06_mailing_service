b56ad72a9cec77b068317efa6ba101e2577c52d2
Valera Demin <wahrwolffe@gmail.com>
2024-01-14 06:20:12 +0300

    add password_change / password_reset / password_reset/done/ / reset / reset/done/

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,7 @@ from users.models import User
 from django import forms
 
 
-class StyleFormMiXin:
+class StyleFormMiXin(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -33,3 +33,30 @@ class UserUpdateForm(StyleFormMiXin, UserChangeForm):
         fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'password')
 
     password = forms.CharField(label='reset', max_length=256, widget=forms.HiddenInput())
+
+
+class UserPasswordForm(StyleFormMiXin, forms.Form):
+    old_password = forms.CharField(widget=forms.PasswordInput, label='Старый пароль')
+    new_password1 = forms.CharField(widget=forms.PasswordInput, label='Новый пароль')
+    new_password2 = forms.CharField(widget=forms.PasswordInput, label='Подтверждение нового пароля')
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password1 = cleaned_data.get('new_password1')
+        new_password2 = cleaned_data.get('new_password2')
+
+        if new_password1 and new_password2 and new_password1 != new_password2:
+            raise forms.ValidationError('пароли не совпадают')
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        password = self.cleaned_data.get('new_password1')
+        self.user.set_password(password)
+        if commit:
+            self.user.save()
+        return self.user
--- users/views.py
+++ users/views.py
@@ -2,14 +2,17 @@ from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView as BaseLoginView
+from django.shortcuts import redirect
+from django.contrib.auth.views import LoginView as BaseLoginView, PasswordChangeView, PasswordResetConfirmView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.contrib.auth.views import PasswordResetView as BasePasswordResetView
+from django.contrib.auth.views import PasswordResetView as BasePasswordResetDoneView
+from django.contrib.auth.views import PasswordResetView as BasePasswordResetCompleteView
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, TemplateView, ListView, UpdateView, DetailView, DeleteView
 
 from app_mailing.views import BaseContextMixin
-from users.forms import LoginViewForm, UserRegisterForm, UserUpdateForm
+from users.forms import LoginViewForm, UserRegisterForm, UserUpdateForm, UserPasswordForm
 from users.models import User
 
 
@@ -110,6 +113,38 @@ class UserDeleteView(BaseContextMixin, DeleteView):
         'title': 'Удаление пользователя',
         'phrases': BaseContextMixin.phrases,
     }
+
+
+class UserPasswordChangeView(BaseContextMixin, PasswordChangeView):
+    form_class = UserPasswordForm
+    template_name = 'users/password_change.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy('users:user_update', kwargs={'pk': self.request.user.pk})
+
+
+class PasswordResetView(BasePasswordResetView):
+    template_name = 'users/password_reset_form.html'
+    email_template_name = 'users/password_reset_email.html'
+    from_email = settings.EMAIL_HOST_USER
+    success_url = reverse_lazy('users:password_reset_done')
+
+
+class PasswordResetDoneView(BasePasswordResetDoneView):
+    template_name = 'users/password_reset_done.html'
+
+
+class MyPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'users/password_reset_confirm.html'
+    success_url = reverse_lazy('users:password_reset_complete')
+
+
+class PasswordResetCompleteView(BasePasswordResetCompleteView):
+    template_name = 'users/password_reset_complete.html'
+    success_url = reverse_lazy('users:login')
 
 
 def get_verification(request, verification_code):
